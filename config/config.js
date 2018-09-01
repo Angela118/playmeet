f11e37a79b4aef79243fa7ce682b2ae5639d8000
Angela <dlswn94@naver.com>
2018-09-01 19:03:12 +0900

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+	server_port: 3000,
+	db_url: 'mongodb://localhost:27017/local',
+	db_schemas: [
+	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'},
+		{file:'./chat_schema', collection:'chatting', schemaName:'ChatSchema',
+		modelName:'ChatModel'},
+		{file:'./appointment_schema', collection:'appointment', schemaName:'AppointmentSchema', modelName:'AppointmentModel'},
+		{file:'./application_schema', collection:'application', schemaName:'ApplicationSchema', modelName:'ApplicationModel'},
+		{file:'./match_schema', collection:'match', schemaName:'MatchSchema', modelName:'MatchModel'}
+	],
+	route_info: [
+		
+	]
+}
