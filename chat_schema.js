16da12f2ed84dafd8476b4f7afa6d5adff477f20
J <41726123+JungASHIN@users.noreply.github.com>
2018-09-06 17:53:41 +0900

    Add files via upload

---
+++ chat_schema.js
@@ -0,0 +1,29 @@
+
+var Schema = {};
+
+Schema.createSchema = function(mongoose) {
+	
+	// 스키마 정의
+	var ChatSchema = mongoose.Schema({
+        email: {type: String, 'default':''},
+        teamname: {type: String, index: 'hashed', 'default':''},
+        message: {type: String},
+        recipient: {type: String, 'default':''},
+        created_at: {type: Date, index: {unique: false}, 'default': Date.now}		
+	});
+	
+
+	// 스키마에 static 메소드 추가
+	// 모든 커피숍 조회
+	// 모델 객체에서 사용할 수 있는 메소드 정의
+	ChatSchema.static('findByEmail', function(email, callback) {
+		return this.find({email:email}, callback);
+	});
+
+	console.log('ChatSchema 정의함.');
+
+	return ChatSchema;
+};
+
+// module.exports에 Schema 객체 직접 할당
+module.exports = Schema;
