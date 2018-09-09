eb38b55ffdd1f8d0888bdc87e0f672b3f84bdd4a
sumin <sumincathy@naver.com>
2018-09-09 18:51:47 +0900

    기본 웹뷰

---
+++ app/src/main/java/com/playmeet/playmeet/MainActivity.java
@@ -0,0 +1,49 @@
+package com.playmeet.playmeet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+
+//        mWebView.goBack();
+//        mWebView.reload();
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+//        mWebView.setWebChromeClient(new WebChromeClient() {
+//            @Override
+//            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+//                return super.onJsAlert(view, url, message, result);
+//            }
+//        });
+//        webSettings.setLoadWithOverviewMode(true);
+//        webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.loadUrl("http://172.30.1.50:3000/login");
+    }
+}
+
