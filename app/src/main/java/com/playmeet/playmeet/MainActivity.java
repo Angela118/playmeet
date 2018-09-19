7745f382001d89d3a977fb5b466e0c42872b0b58
sumin <sumincathy@naver.com>
2018-09-20 00:19:26 +0900

    mToken test

--- app/src/main/java/com/playmeet/playmeet/MainActivity.java
+++ app/src/main/java/com/playmeet/playmeet/MainActivity.java
@@ -1,41 +1,93 @@
 package com.playmeet.playmeet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+//fcm
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MainActivity extends AppCompatActivity {
+    EditText messageInput;
+    TextView messageOutput;
+    TextView log;
 
     WebView mWebView;
+    String regId;
+
+    RequestQueue queue;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView) findViewById(R.id.webView);
+//        mWebView = (WebView) findViewById(R.id.webView);
 
-//        mWebView.goBack();
-//        mWebView.reload();
+        //fcm
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        FirebaseInstanceId.getInstance().getToken();
 
-/*        mWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                return super.onJsAlert(view, url, message, result);
-            }
-        });*/
-
-        WebSettings webSettings = mWebView.getSettings();
+        /*WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setLoadWithOverviewMode(true);
-        webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);*/
 
+        messageInput = (EditText) findViewById(R.id.messageInput);
+        messageOutput = (TextView) findViewById(R.id.messageOutput);
+        log = (TextView) findViewById(R.id.log);
+
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = messageInput.getText().toString();
+                send(input);
+            }
+        });
+
+        queue = Volley.newRequestQueue(getApplicationContext());
+
+//        getRegistrationId();
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( MainActivity.this,  new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String mToken = instanceIdResult.getToken();
+                Log.d("Token",mToken);
+                println("mToken : " + mToken);
+            }
+        });
+
+
+/*
         //alert 대응
         mWebView.setWebChromeClient(new WebChromeClient(){
             @Override
@@ -56,7 +108,143 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mWebView.loadUrl("http://192.168.19.168:3000/login");
+        mWebView.loadUrl("http://192.168.19.138:3000/login");*/
+
+
+    }
+
+
+/*    public void getRegistrationId() {
+        println("getRegistrationId() 호출됨.");
+
+//        regId = FirebaseInstanceId.getInstance().getToken();
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( MainActivity.this,  new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String mToken = instanceIdResult.getToken();
+                Log.e("Token",mToken);
+                regId = mToken;
+                println("regId : " + regId);
+//                println("regId : " + mToken);
+//                println("test1");
+            }
+        });
+    }*/
+
+    public void send(String input) {
+
+        JSONObject requestData = new JSONObject();
+
+        try {
+            requestData.put("priority", "high");
+
+            JSONObject dataObj = new JSONObject();
+            dataObj.put("contents", input);
+            requestData.put("data", dataObj);
+
+            JSONArray idArray = new JSONArray();
+            idArray.put(0, regId);
+            requestData.put("registration_ids", idArray);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        sendData(requestData, new SendResponseListener() {
+            @Override
+            public void onRequestCompleted() {
+                println("onRequestCompleted() 호출됨.");
+            }
+
+            @Override
+            public void onRequestStarted() {
+                println("onRequestStarted() 호출됨.");
+            }
+
+            @Override
+            public void onRequestWithError(VolleyError error) {
+                println("onRequestWithError() 호출됨.");
+            }
+        });
+
+    }
+
+    public interface SendResponseListener {
+        public void onRequestStarted();
+        public void onRequestCompleted();
+        public void onRequestWithError(VolleyError error);
+    }
+
+    public void sendData(JSONObject requestData, final SendResponseListener listener) {
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                "https://fcm.googleapis.com/fcm/send",
+                requestData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        listener.onRequestCompleted();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.onRequestWithError(error);
+            }
+        }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers = new HashMap<String,String>();
+                headers.put("Authorization"
+                        ,"key=AAAACgDmJXk:APA91bHwNjQcIeWEkiLuOKQWJKNzsiWbh5Fk0CfpUwSXHRv9t-acdcGJvZ_NBAkvSFex3nUwEU0QpOnQteczkP6ry-x_7x1_Y9odqxTgY3ijJ6vUUFBdEWtBcsZsbJGMO46wx9cMA-BP");
+
+                return headers;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+
+        request.setShouldCache(false);
+        listener.onRequestStarted();
+        queue.add(request);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        println("onNewIntent() called.");
+
+        if (intent != null) {
+            processIntent(intent);
+        }
+
+        super.onNewIntent(intent);
+    }
+
+
+    private void processIntent(Intent intent) {
+        String from = intent.getStringExtra("from");
+        if (from == null) {
+            println("from is null.");
+            return;
+        }
+
+        String contents = intent.getStringExtra("contents");
+
+        println("DATA : " + from + ", " + contents);
+        messageOutput.setText("[" + from + "]로부터 수신한 데이터 : " + contents);
+    }
+
+    public void println(String data){
+        log.append(data + "\n");
     }
 }
-
