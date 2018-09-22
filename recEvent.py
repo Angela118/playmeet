1b9192f5f44edf26423340c71280f109bd9a73df
Angela <dlswn94@naver.com>
2018-09-22 16:32:59 +0900

    추천시스템 수정

--- recEvent.py
+++ recEvent.py
@@ -15,7 +15,7 @@ def swap(x, i, j):
 
 
 #파일 불러오기 
-file = open('C:/Users/user/brackets_nodejs/server/recEvent.csv', 'r', encoding='UTF8')
+file = open('C:/Users/user/brackets_nodejs/playmeet/recEvent.csv', 'r', encoding='UTF8')
 reader = csv.reader(file)
 
 data=[]
@@ -24,22 +24,36 @@ for row_list in reader:
   
 file.close()
 
-print(data)
-print()
 
 header = []
 header = data[0]
 
 del data[0]
 
+original_data = data.copy()
 
 
-#scaling
+print(original_data)
+print()  
+
+
+original_review=[]
 for i in range(0, len(data)):
-    data[i][4] = data[i][4] + '0'
+    original_review.append([data[i][8],data[i][8]])
 
-        
-dat = data[:]
+print(original_review)
+print()
+
+#리뷰 없는 경우 처리
+for i in range(0, len(data)):
+    if(data[i][8] == "리뷰없음"):
+        data[i][8] = '3'
+        if(data[i][9] == "리뷰없음"):
+            data[i][9] = '3'
+
+print(data)
+print()   
+
     
     
 #data의 id값 
@@ -50,17 +64,9 @@ for i in range(1, len(data)):
     del data[i][0]
 
 
-if(len(data)>1):        
-    if(type(data[1][8]) == 'str'):
-        data = np.array(data)
-        data = data[:, 0:9]
-    else:
-        data = np.array(data)
-        data = data[:, 0:7]
-else:
-    data = np.array(data)
-    data = data[:, 0:7]
-    
+data = np.array(data)
+data = data[:, 0:9]
+
 
 print(data)
 print()
@@ -68,6 +74,12 @@ print()
 
 data = data.astype('float64')
 
+
+#scaling
+for i in range(0, len(data)):
+    data[i][0] = data[i][0] / 10
+    data[i][1] = data[i][1] / 10
+    data[i][5] = data[i][5] * 10
 
 print(data)
 print()
@@ -83,6 +95,7 @@ print()
 ar_euc = []
 for i in range(0, len(euc_dst)):
     ar_euc.append([i+1, euc_dst[i]])
+    
 print(ar_euc)
 print()
 
@@ -93,30 +106,30 @@ for size in reversed(range(len(ar_euc))):
     for i in range(size):
         if ar_euc[i][1]>ar_euc[i+1][1]:
             swap(ar_euc, i, i+1)
-                
 
+print(ar_euc)
+print()
+                
 result_data = []
 for i in range(0, len(ar_euc)):
-    result_data.append(dat[ar_euc[i][0]])
+    result_data.append(original_data[ar_euc[i][0]])
     result_data[i].insert(0, idArray[ar_euc[i][0]-1])
+    result_data[i][8] = original_review[i+1][0]
+    result_data[i][9] = original_review[i+1][1]
 
-
-#scaling 되돌리기
-for i in range(0, len(result_data)):
-    result_data[i][4] = result_data[i][4][0:len(result_data[i][4])-1]
-
+print(result_data)
+print()
 
 result_data.insert(0, header)
 
 
-print(result_data)
-print()
+
 
 
 
 
 #recOutput.csv에 저장 
-with open('C:/Users/user/brackets_nodejs/server/recOutput.csv','w', encoding='UTF8', newline='') as output:
+with open('C:/Users/user/brackets_nodejs/playmeet/recOutput.csv','w', encoding='UTF8', newline='') as output:
     writer = csv.writer(output)
     for val in result_data:
         writer.writerow(val)
