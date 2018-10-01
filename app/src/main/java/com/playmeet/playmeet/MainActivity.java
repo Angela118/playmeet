24c27f6b7c340152a8365036a186754648d67968
sumin <sumincathy@naver.com>
2018-10-01 18:15:47 +0900

    웹뷰 파일 업로드

--- app/src/main/java/com/playmeet/playmeet/MainActivity.java
+++ app/src/main/java/com/playmeet/playmeet/MainActivity.java
@@ -1,9 +1,16 @@
 package com.playmeet.playmeet;
 
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.JsResult;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,6 +23,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
+import java.io.File;
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -26,10 +34,16 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-
 public class MainActivity extends AppCompatActivity {
     WebView mWebView;
     String mToken;
+
+    private ValueCallback<Uri> filePathCallbackNormal;
+    private ValueCallback<Uri[]> filePathCallbackLollipop;
+    private final static int FILECHOOSER_NORMAL_REQ_CODE = 1;
+    private final static int FILECHOOSER_LOLLIPOP_REQ_CODE = 2;
+    private Uri mCapturedImageURI;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        webSettings.setBuiltInZoomControls(true);
 
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( MainActivity.this,  new OnSuccessListener<InstanceIdResult>() {
             @Override
@@ -52,8 +67,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //alert 대응
-        mWebView.setWebChromeClient(new WebChromeClient(){
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            // For Android < 3.0
+            public void openFileChooser( ValueCallback<Uri> uploadMsg) {
+                Log.d("MainActivity", "3.0 <");
+                openFileChooser(uploadMsg, "");
+            }
+
+            // For Android 3.0+
+            public void openFileChooser( ValueCallback<Uri> uploadMsg, String acceptType) {
+                Log.d("MainActivity", "3.0+");
+                filePathCallbackNormal = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                // i.setType("video/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_NORMAL_REQ_CODE);
+            }
+
+            // For Android 4.1+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                Log.d("MainActivity", "4.1+");
+                openFileChooser(uploadMsg, acceptType);
+            }
+
+            // For Android 5.0+
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    WebChromeClient.FileChooserParams fileChooserParams) {
+                Log.d("MainActivity", "5.0+");
+                if (filePathCallbackLollipop != null) {
+                    filePathCallbackLollipop.onReceiveValue(null);
+                    filePathCallbackLollipop = null;
+                }
+                filePathCallbackLollipop = filePathCallback;
+
+                // Create AndroidExampleFolder at sdcard
+                File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "AndroidExampleFolder");
+                if (!imageStorageDir.exists()) {
+                    // Create AndroidExampleFolder at sdcard
+                    imageStorageDir.mkdirs();
+                }
+
+                // Create camera captured image file path and name
+                File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                mCapturedImageURI = Uri.fromFile(file);
+
+                Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+
+
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+//                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_LOLLIPOP_REQ_CODE);
+
+
+// Create file chooser intent
+                Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
+                // Set camera intent to file chooser
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{captureIntent});
+
+                // On select image call onActivityResult method of activity
+                startActivityForResult(chooserIntent, FILECHOOSER_LOLLIPOP_REQ_CODE);
+
+                return true;
+            }
+
             @Override
             public boolean onJsAlert(WebView view, String url, String message, JsResult result)
             {
@@ -64,17 +144,46 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mWebView.setWebViewClient(new WebViewClient() {
-
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
         });
+        mWebView.loadUrl("http://10.10.0.239:3000/login");
 
-        mWebView.loadUrl("http://10.100.9.220:3000/login");
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == FILECHOOSER_NORMAL_REQ_CODE) {
+                if (filePathCallbackNormal == null) return ;
+                Uri result = (data == null || resultCode != RESULT_OK) ? null : data.getData();
+                filePathCallbackNormal.onReceiveValue(result);
+                filePathCallbackNormal = null;
+            } else if (requestCode == FILECHOOSER_LOLLIPOP_REQ_CODE) {
+                /*if (filePathCallbackLollipop == null) return ;
+                filePathCallbackLollipop.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+                filePathCallbackLollipop = null;*/
+
+                Uri[] result = new Uri[0];
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    if(resultCode == RESULT_OK){
+                        result = (data == null) ? new Uri[]{mCapturedImageURI} : WebChromeClient.FileChooserParams.parseResult(resultCode, data);
+                    }
+                    filePathCallbackLollipop.onReceiveValue(result);
+                }
+
+
+            }
+        } else {
+            if (filePathCallbackLollipop != null) {
+                filePathCallbackLollipop.onReceiveValue(null);
+                filePathCallbackLollipop = null;
+            }
+        }
+    }
 
     public void updateUserInfo(){
         OkHttpClient client = new OkHttpClient();
@@ -84,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Request request = new Request.Builder()
-                .url("http://10.100.9.220:3000/token")
+                .url("http://10.10.0.239:3000/token")
                 .post(formBody)
                 .build();
 
